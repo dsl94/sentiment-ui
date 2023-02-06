be71b892cdba6e530a91727eda38d7ae2074e64c
nemanja <nemanja.petrovic@united.cloud>
2023-02-06 15:29:29 +0100

    Visualization

--- src/app/dto/analyzer-response.dto.ts
+++ src/app/dto/analyzer-response.dto.ts
@@ -1,9 +1,12 @@
 import {AnalyzedTweet} from "./analyzed-tweet.dto";
 import {ResultsPerLibrary} from "./result-per-livrary.dto";
 import {SentimentPerLibrary} from "./sentiment-per-library.dto";
+import {Fscore} from "./fscore.dto";
 
 export interface AnalyzerResponse {
   analyzed_tweets: AnalyzedTweet[];
   results_per_library: ResultsPerLibrary;
   sentiment_per_library: SentimentPerLibrary;
+
+  f_score: Fscore[];
 }
---
+++ src/app/dto/fscore.dto.ts
@@ -0,0 +1,7 @@
+export interface Fscore {
+  TP: number;
+  FP: number;
+  TN: number;
+  FN: number;
+  F_SCORE: number;
+}
